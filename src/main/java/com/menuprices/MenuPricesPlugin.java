6ba4cdba433dc7481cc0a123ff22586baaa14585
Hero <caleb@calebgj.io>
2025-08-06 12:13:32 -0500

    finish the plugin

--- src/main/java/com/menuprices/MenuPricesPlugin.java
+++ src/main/java/com/menuprices/MenuPricesPlugin.java
@@ -2,15 +2,19 @@ package com.menuprices;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
+
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.*;
+import net.runelite.api.events.MenuOpened;
+import net.runelite.api.gameval.InterfaceID;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetUtil;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.game.ItemManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.util.LinkBrowser;
 
 @Slf4j
 @PluginDescriptor(
@@ -23,6 +27,9 @@ public class MenuPricesPlugin extends Plugin
 
 	@Inject
 	private MenuPricesConfig config;
+
+	@Inject
+	private ItemManager itemManager;
 
 	@Override
 	protected void startUp() throws Exception
@@ -37,11 +44,26 @@ public class MenuPricesPlugin extends Plugin
 	}
 
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	public void onMenuOpened(MenuOpened event)
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+		final MenuEntry[] entries = event.getMenuEntries();
+		for (int idx = entries.length - 1; idx >= 0; --idx)
 		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+			final MenuEntry entry = entries[idx];
+			final Widget w = entry.getWidget();
+			final int canonItemId = itemManager.canonicalize(entry.getItemId());
+
+			if (w != null && WidgetUtil.componentToInterface(w.getId()) == InterfaceID.INVENTORY && "Examine".equals(entry.getOption()) && entry.getIdentifier() == 10) {
+				if (itemManager.getItemComposition(canonItemId).isTradeable()) {
+					client.getMenu().createMenuEntry(idx)
+						.setOption("Open Wiki Prices")
+						.setTarget(entry.getTarget())
+						.setType(MenuAction.RUNELITE)
+						.onClick(consumer -> {
+							LinkBrowser.browse("https://prices.runescape.wiki/osrs/item/" + canonItemId);
+						});
+				}
+			}
 		}
 	}
 
